6340994ee8dbaa0ee564584df8197c7ce42bbf2e
Avi Cohen <avicohen@Avis-MacBook-Pro.local>
2020-12-10 22:53:06 +0200

    cards memory game

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Board from './Board'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🧠Test your memory🧠</h1>
+      <Board />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Board.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import Card from './Card'
+import './Board.css'
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        const fronts = [
+            '🚌',
+            '🌍',
+            '🌸',
+            '🍰',
+            '🏀',
+            '🎉',
+            // '🐳',
+            // '🌈',
+            // '🎹',
+            // '🍔',
+            // '🍺',
+            // '🔥'
+        ]
+        const deck = fronts
+            .concat(fronts)
+            .sort(() => Math.random - 0.5)
+            .map(f => {
+                return {
+                    content: f,
+                    faceUp: false
+                }
+            })
+        this.state = {
+            deck: deck,
+            firstCard: null,
+        }
+
+    }
+
+    flipCardTo(cardIdx, faceUp) {
+        this.setState({
+            deck: this.state.deck.map((f, i) => {
+                if (i === cardIdx) {
+                    return {
+                        content: f.content,
+                        faceUp: !f.faceUp
+                    }
+                } else {
+                    return f
+                }
+            })
+        })
+    }
+
+    flip(cardIdx) {
+        if (this.state.firstCard === null) {
+            this.setState({ firstCard: cardIdx })
+        } else {
+            const firstCardContent = this.state.deck[this.state.firstCard].content
+            const secondCardContent = this.state.deck[cardIdx].content
+            if (firstCardContent === secondCardContent) {
+                this.setState({firstCard: null})
+            } else {
+                setTimeout(() => {
+                    this.flipCardTo(this.state.firstCard, false)
+                    this.flipCardTo(cardIdx, false)
+                    this.setState({firstCard: null})
+                }, 1200)
+            }
+        }
+
+        this.flipCardTo(cardIdx, !this.state.deck[cardIdx].faceUp)
+    }
+
+    render() {
+        return (
+            this.state.deck.map((f, i) => {
+                return (<div className="Board">
+                    <Card
+                        flip={() => { this.flip(i) }}
+                        content={f.content}
+                        faceUp={f.faceUp} />
+                </div>)
+            })
+        )
+    }
+}
+
+export default Board
